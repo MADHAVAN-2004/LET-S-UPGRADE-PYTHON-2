566c98e8fddc1a20006a1ef4823fd829bfb479b3
MADHAVAN-2004 <97903056+MADHAVAN-2004@users.noreply.github.com>
2022-01-18 08:01:59 -0800

    Add files via upload

---
+++ lets upgrade assignment python day 2.py
@@ -0,0 +1,22 @@
+print(" I am going to play with the variables!!!")
+a=1
+b=1.5
+c="let us upgrade"
+d='a'
+e=True
+g=[1,2,3,4,5]
+h=(1,2,3,4,5)
+i={1,2,3,3,3,3,3}
+j={'name':"MADHAVAN",'profession':"STUDENT"}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print("value of a is:",a,"\nvalue of b is:",b)
+print("value of a is: %s and \n value of b is: %s"%(a,b))
+print("value of b is: {1} and \ns value of b is: {0}".format(a,b))
